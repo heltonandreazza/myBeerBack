8ca099a4c7cff4d450b939691a8b9a901a686ab6
Helton Andreazza <helton.prg@gmail.com>
2017-04-28 01:13:53 -0300

    yeah

--- app/config/index.js
+++ app/config/index.js
@@ -4,9 +4,12 @@ if (process.env.NODE_ENV === 'production') {
   console.log('heroku prod', process.env);
   //heroku enviroment variables
   module.exports = {
-    host: process.env.host || "",
-    dbURI: process.env.dbURI,
-    sessionSecret: process.env.sessionSecret
+    // host: process.env.host || "",
+    // dbURI: process.env.dbURI,
+    // sessionSecret: process.env.sessionSecret
+    "host": "http://localhost:3000",
+    "dbURI": "mongodb://econback:econback@ds149567.mlab.com:49567/econback",
+    "sessionSecret": "potato"    
   }
 } else {
   console.log('dev');
